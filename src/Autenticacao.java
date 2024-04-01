61762377921c99536ca5f4a8706fb0bf78718e6f
Tekislla <jplazarim@gmail.com>
2024-04-01 19:57:00 -0300

    Criado sistema de autenticação

---
+++ src/Autenticacao.java
@@ -0,0 +1,28 @@
+public class Autenticacao {
+
+    static final String senhaCorreta = "S3nH49";
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Uso: [aplicativo] [usuario] [senha]");
+            return;
+        }
+
+        String usuario = args[0];
+        String senha = args[1];
+
+        if (autenticar(usuario, senha)) {
+            System.out.println("Acesso concedido");
+        } else {
+            System.out.println("Usuario ou senha invalidos");
+        }
+    }
+
+    public static boolean autenticar(String usuario, String senha) {
+        if (!usuario.equals("administrador")) {
+            return false;
+        }
+
+        return senha.equals(senhaCorreta);
+    }
+}
